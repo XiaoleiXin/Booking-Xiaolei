6943860ae22860d3165ca9ec4ec472a91417e78c
XiaoleiXin <37600641+XiaoleiXin@users.noreply.github.com>
2018-06-08 22:03:55 -0700

    add the summary component and mock data.

--- client/src/components/guests.jsx
+++ client/src/components/guests.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
+import Calendar from './calendar.jsx';
 import { Collapse, Well } from 'react-bootstrap';
 
 class Guests extends React.Component {
@@ -17,7 +18,14 @@ class Guests extends React.Component {
       infantsAddStatus: false,
       infantsRemoveStatus: true,
       open: false,
+      price: 205,
+      days: null,
+      cleaning: 30,
     };
+  }
+
+  setTotalDays(days) {
+    this.setState({ days });
   }
 
   setAddButtonState(adults, children) {
@@ -104,51 +112,66 @@ class Guests extends React.Component {
   }
 
   render() {
+    const totalPrice = this.state.price * this.state.days;
+    const serviceFee = Math.floor(totalPrice * 0.09);
+    const cleaningFee = this.state.cleaning;
+    const total = totalPrice + serviceFee + cleaningFee;
     const totalGuests = this.state.adults + this.state.children;
     const totalPeople = this.state.infants ? `${totalGuests} guests, ${this.state.infants} infants` : `${totalGuests} guests`;
     return (
-      <div className="guestModule">
-        <span id="wordGuests" >Guests</span>
-        <button type="button" id="guestButton" className="btn btn-Primary btn-block" onClick={() => this.setState({ open: !this.state.open })}>
-          {totalPeople}
-        </button>
-        <Collapse id="collapse" in={this.state.open}>
-          <div>
-            <Well id="well">
-              <div className="adults">
-                <div id="adults">Adults</div>
-                <button id="adultsbutton" className="btn btn-circle btn-lg am" type="button" onClick={() => this.addAdults()} disabled={this.state.adultAddStatus}>+</button>
-                <span id="adultsletters" className="am">{this.state.adults}</span>
-                <button id="adultsbutton" className="btn btn-circle btn-lg am" type="button" onClick={() => this.removeAdults()} disabled={this.state.adultRemoveStatus}>-</button>
+      <div>
+        <Calendar setTotalDays={this.setTotalDays.bind(this)} />
+        <div className="guestModule">
+          <span id="wordGuests" >Guests</span>
+          <button type="button" id="guestButton" className="btn btn-Primary btn-block" onClick={() => this.setState({ open: !this.state.open })}>
+            {totalPeople}
+          </button>
+          <Collapse id="collapse" in={this.state.open}>
+            <div>
+              <Well id="well">
+                <div className="adults">
+                  <div id="adults">Adults</div>
+                  <button id="adultsbutton" className="btn btn-circle btn-lg am" type="button" onClick={() => this.addAdults()} disabled={this.state.adultAddStatus}>+</button>
+                  <span id="adultsletters" className="am">{this.state.adults}</span>
+                  <button id="adultsbutton" className="btn btn-circle btn-lg am" type="button" onClick={() => this.removeAdults()} disabled={this.state.adultRemoveStatus}>-</button>
+                </div>
+                <div className="guests">
+                  <span>Children</span>
+                  <button className="btn btn-circle btn-lg am" type="button" onClick={() => this.addChildren()} disabled={this.state.childrenAddStatus}>+</button>
+                  <span id="amletters" className="am">{this.state.children}</span>
+                  <button className="btn btn-circle btn-lg am" type="button" onClick={() => this.removeChildren()} disabled={this.state.childrenRemoveStatus}>-</button>
+                  <p id="under">Ages 2 - 12</p>
+                </div>
+                <div className="guests">
+                  <span>Infants</span>
+                  <button className="btn btn-circle btn-lg am" type="button" onClick={() => this.addInfants()} disabled={this.state.infantsAddStatus}>+</button>
+                  <span id="amletters" className="am">{this.state.infants}</span>
+                  <button className="btn btn-circle btn-lg am" type="button" onClick={() => this.removeInfants()} disabled={this.state.infantsRemoveStatus}>-</button>
+                  <p id="under">Under 2</p>
+                </div>
+                <div id="limit">5 guests maximum. Infants don’t count toward the number of guests.</div>
+                <a href="#;" id="close" onClick={() => this.setState({ open: !this.state.open })}>Close</a>
+              </Well>
+            </div>
+          </Collapse>
+          {this.state.days ?
+            <div id="summary" >
+              <div className="line">${this.state.price} x {this.state.days} night
+                <span className="numbers">${totalPrice}</span>
               </div>
-              <div className="guests">
-                <span>Children</span>
-                <button className="btn btn-circle btn-lg am" type="button" onClick={() => this.addChildren()} disabled={this.state.childrenAddStatus}>+</button>
-                <span id="amletters" className="am">{this.state.children}</span>
-                <button className="btn btn-circle btn-lg am" type="button" onClick={() => this.removeChildren()} disabled={this.state.childrenRemoveStatus}>-</button>
-                <p id="under">Ages 2 - 12</p>
+              <div className="line">Cleaning fee
+                <span className="numbers">${cleaningFee}</span>
               </div>
-              <div className="guests">
-                <span>Infants</span>
-                <button className="btn btn-circle btn-lg am" type="button" onClick={() => this.addInfants()} disabled={this.state.infantsAddStatus}>+</button>
-                <span id="amletters" className="am">{this.state.infants}</span>
-                <button className="btn btn-circle btn-lg am" type="button" onClick={() => this.removeInfants()} disabled={this.state.infantsRemoveStatus}>-</button>
-                <p id="under">Under 2</p>
+              <div className="line">Service fee
+                <span className="numbers">${serviceFee}</span>
               </div>
-              <div id="limit">5 guests maximum. Infants don’t count toward the number of guests.</div>
-              <a href="#;" id="close" onClick={() => this.setState({ open: !this.state.open })}>Close</a>
-            </Well>
-          </div>
-        </Collapse>
-        <div id="summary"> 
-        {/* The following is just for testing the css. Will change them to real summay. */}
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
-          <div>123</div>
+              <div id="total" className="line">Total
+                <span className="numbers">${total}</span>
+              </div>
+            </div> : <div id="space"> .</div>}
+          <button type="button" id="book" className="btn btn-Primary btn-danger btn-block">Book</button>
+          <div id="wont" className="chargeSentence">You won’t be charged yet</div>
         </div>
-        <button type="button" id="book" className="btn btn-Primary btn-danger btn-block">Book</button>
-        <div id="wont" className="chargeSentence">You won’t be charged yet</div>
       </div>
     );
   }
